3d32c5b0f6bafab5608a79a9b7185b800e21de0c
wuhonglei <1017368065@qq.com>
2025-11-23 23:04:01 +0800

    feat: 添加 Aegis 前端监控脚本和类型定义

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -10,3 +10,29 @@ interface ImportMetaEnv {
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
+
+// 声明 gtag 函数的类型定义
+declare function gtag(
+  type: "event",
+  customEventName: string,
+  fieldObject: {
+    event_category?: string;
+    event_label?: number | string;
+    debug_mode?: boolean;
+    value: number | string;
+  }
+): void;
+
+/**
+ * 腾讯云监控团队提供的前端监控 aegis 类型定义
+ * 文档: https://www.npmjs.com/package/aegis-web-sdk
+ */
+declare namespace aegis {
+  interface Params {
+    name: string; // "XXX请求成功"; 必填
+    ext1?: string | number; // "额外参数1"
+    ext2?: string | number; // "额外参数2"
+    ext3?: string | number; // "额外参数3"
+  }
+  function reportEvent(params: Params): void;
+}
