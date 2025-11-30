480ab322a971d61365e679d28078b2029a98045d
wuhonglei <1017368065@qq.com>
2025-11-30 22:48:12 +0800

    chore: 修复环境变量加载方式，使用 loadEnv 函数

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,53 +1,59 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    svgr({
-      svgrOptions: {
-        icon: true, // 让 SVG 自适应 (width/height = 1em)
-      },
-    }),
-  ],
-  server: {
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: process.env.VITE_PROXY_TARGET || "http://localhost:8000",
-        changeOrigin: true,
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          // 进一步拆分大块依赖，避免单 chunk 内存占用过高
-          vendor: ["react", "react-dom"],
-          utils: ["axios", "lodash-es", "dayjs", "uuid", "mitt"],
-          ui: ["simplebar-react"],
-          components: ["@ant-design/x", "@ant-design/icons", "antd"],
-          markdown: [
-            "react-markdown",
-            "react-syntax-highlighter",
-            "rehype-external-links",
-            "rehype-raw",
-            "remark-gfm",
-          ],
+export default defineConfig(({ mode }) => {
+  // 加载环境变量
+  const env = loadEnv(mode, process.cwd(), "");
+  console.info("VITE_PROXY_TARGET:", env.VITE_PROXY_TARGET);
+
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+      svgr({
+        svgrOptions: {
+          icon: true, // 让 SVG 自适应 (width/height = 1em)
+        },
+      }),
+    ],
+    server: {
+      port: 3000,
+      proxy: {
+        "/api": {
+          target: env.VITE_PROXY_TARGET || "http://localhost:8000",
+          changeOrigin: true,
         },
       },
     },
-  },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    build: {
+      sourcemap: false,
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            // 进一步拆分大块依赖，避免单 chunk 内存占用过高
+            vendor: ["react", "react-dom"],
+            utils: ["axios", "lodash-es", "dayjs", "uuid", "mitt"],
+            ui: ["simplebar-react"],
+            components: ["@ant-design/x", "@ant-design/icons", "antd"],
+            markdown: [
+              "react-markdown",
+              "react-syntax-highlighter",
+              "rehype-external-links",
+              "rehype-raw",
+              "remark-gfm",
+            ],
+          },
+        },
+      },
+    },
+  };
 });
