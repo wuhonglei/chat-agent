0856fca32e3a775c42f35232d2c5d1b740f1e2c8
wuhonglei <1017368065@qq.com>
2025-12-20 20:37:38 +0800

    refactor: 更新 Vite 配置，修改组件 Schema 输出路径

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig(({ mode }) => {
         inputPath: "src/componentTools/index.ts",
         outputDirs: [
           "src/componentTools/component-schemas",
-          "../backend/data/component-schemas",
+          "public/component-schemas",
         ],
       }), // 在构建时自动生成组件 JSON Schema
     ],
