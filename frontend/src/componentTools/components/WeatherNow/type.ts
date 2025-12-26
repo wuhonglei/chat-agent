1873ad40527a2c98db6abf96c7dfbe2a63a3f4c2
Honglei Wu <honglei.wu@shopee.com>
2025-12-26 17:39:28 +0800

    refactor: 更新天气组件数据类型，调整属性为可选

--- frontend/src/componentTools/components/WeatherNow/type.ts
+++ frontend/src/componentTools/components/WeatherNow/type.ts
@@ -5,34 +5,34 @@
 export interface WeatherNowData {
   /** 观测时间 */
   obsTime: string;
-  /** 温度 */
-  temp: string;
-  /** 体感温度 */
-  feelsLike: string;
   /** 天气图标代码 */
   icon: string;
+  /** 温度 */
+  temp?: string;
   /** 天气状况文字描述 */
-  text: string;
+  text?: string;
+  /** 体感温度 */
+  feelsLike?: string;
   /** 风向360度 */
-  wind360: string;
+  wind360?: string;
   /** 风向 */
-  windDir: string;
+  windDir?: string;
   /** 风力等级 */
-  windScale: string;
+  windScale?: string;
   /** 风速 */
-  windSpeed: string;
+  windSpeed?: string;
   /** 相对湿度 */
-  humidity: string;
+  humidity?: string;
   /** 降水量 */
-  precip: string;
+  precip?: string;
   /** 大气压强 */
-  pressure: string;
+  pressure?: string;
   /** 能见度 */
-  vis: string;
+  vis?: string;
   /** 云量 */
-  cloud: string;
+  cloud?: string;
   /** 露点温度 */
-  dew: string;
+  dew?: string;
 }
 
 /**
