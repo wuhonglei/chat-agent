024ce791eaefadebaff424a12483e97efb0843b5
wuhonglei <1017368065@qq.com>
2025-12-27 00:37:05 +0800

    refactor: 添加天气组件到组件工具配置

--- frontend/src/componentTools/index.ts
+++ frontend/src/componentTools/index.ts
@@ -1,8 +1,11 @@
 import { ComponentToolItem } from "@/interfaces";
+import WeatherNow from "./components/WeatherNow";
 
 const componentTools: ComponentToolItem[] = [
   {
     name: "weather",
+    component: WeatherNow,
+    typeSourceFile: "./components/WeatherNow/type.ts",
     whenCondition: "and",
     when: {
       mcp_tool_names: ["weather"],
