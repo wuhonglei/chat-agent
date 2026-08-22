368405c7e444e0862808cc0a1b15c09eab1857d8
wuhonglei <1017368065@qq.com>
2026-08-22 19:04:00 +0800

    style: 为压缩结果模态框中的 MarkdownContainer 添加自定义样式

--- frontend/src/components/Layout/modals/CompressResultModal.tsx
+++ frontend/src/components/Layout/modals/CompressResultModal.tsx
@@ -1,12 +1,18 @@
 import { ConversationCompressResponse } from "@/interfaces";
 import MarkdownContainer from "@/pages/ChatPage/components/MarkdownContainer";
 import { Modal } from "antd";
+import type { CSSProperties } from "react";
 
 type Props = {
   open: boolean;
   result: ConversationCompressResponse | null;
   onClose: () => void;
 };
+
+const summaryMarkdownStyle = {
+  "--font-size": "14px",
+  "--margin-block": "4px",
+} as CSSProperties;
 
 export default function CompressResultModal({ open, result, onClose }: Props) {
   return (
@@ -29,7 +35,9 @@ export default function CompressResultModal({ open, result, onClose }: Props) {
             </span>
           </div>
           <div className="max-h-80 overflow-y-auto rounded-md bg-black/[0.03] p-3">
-            <MarkdownContainer className="w-full">{result.summary}</MarkdownContainer>
+            <MarkdownContainer className="w-full" style={summaryMarkdownStyle}>
+              {result.summary}
+            </MarkdownContainer>
           </div>
         </div>
       ) : null}
